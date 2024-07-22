2f3c1b8e82f5bcd6532d2b17a2f468254e0fb94d
Palloma_Rangel <palloma.silva0018@gmail.com>
2024-07-22 09:28:40 -0300

    inicio do projeto

---
+++ src/test/java/com/generation/crudfarmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.crudfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
